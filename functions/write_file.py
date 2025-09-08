8681ebff56a4603fed0a8d497f01e4fe0191acb2
OdinDovah <pro@albertson-clan.com>
2025-09-07 21:33:30 -0400

    Able to write and create files to disk

---
+++ functions/write_file.py
@@ -0,0 +1,18 @@
+import os
+
+
+def write_file(working_directory, file_path, content):
+    full_path = os.path.join(working_directory, file_path)
+    try:
+        if not os.path.abspath(full_path).startswith(os.path.abspath(working_directory)):
+            return f"Error: Cannot write to '{file_path}' as it is outside the permitted working directory"
+        if not os.path.exists(os.path.dirname(full_path)):
+            os.makedirs(os.path.dirname(full_path))
+            f = open(full_path, "a").write(content)
+            return f"Successfully wrote to '{file_path}' ({len(content)} characters written)"
+        else:
+            with open(full_path, "w") as f:
+                f.write(content)
+            return f"Successfully wrote to '{file_path}' ({len(content)} characters written)"
+    except Exception as e:
+        return f"Error: {str(e)}"
--- tests.py
+++ tests.py
@@ -1,15 +1,11 @@
-from functions.get_file_content import get_file_content
+from functions.write_file import write_file
+# from functions.get_file_content import get_file_content
 
 
 def main():
-    print("Result for main.py")
-    print(get_file_content("calculator", "main.py"))
-    print("Result for pkg/calculator.py")
-    print(get_file_content("calculator", "pkg/calculator.py"))
-    print("Result for /bin/cat")
-    print(get_file_content("calculator", "/bin/cat"))
-    print("Result for non-existing")
-    print(get_file_content("calculator", "pkg/does_not_exist.py"))
+    print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
+    print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+    print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
 
 
 if __name__ == "__main__":
